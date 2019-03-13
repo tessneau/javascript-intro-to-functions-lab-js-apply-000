f9c9babaf06d07abd051aa0a471a4ddc59eb9635
Tess Neau <tessneau@gmail.com>
2019-03-13 20:37:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,4 +19,6 @@ function sayHiToGrandma(string) {
   return "I can't hear you!";
   if(string.toUpperCase() === string)
   return "YES INDEED!";
+  if("I love you, Grandma." === string)
+  return "I love you, too.";
 }
